d69fc3827f4ffc825069870a99b0ee09d40538a2
AlexOuyang <ouyangchenxing@hotmail.com>
2015-08-15 13:48:12 -0700

    fixed external link problems

--- js/angularFilter.js
+++ js/angularFilter.js
@@ -14,9 +14,10 @@ angular.module('ngFilter').controller("MainController", function() {
         title: '★ Quadcopter (In Progress)',
         tags: 'Hardware, Arduino',
         year: 2015,
-        description: "Created and Programmed a Quadcopter with friends.",
+        description: "The purpose of this project is to create a systems and algorithms necessary to allow a quadcopter to autonomously locate and land on a station target using C and Arduino Uno micro-controller. (In progress)",
         img_url: "img/Projects/quadcopter.jpg",
-        url: ""
+        url: "",
+        haveExternalLink: false
     }, {
         title: '✓ Insatiable Spider (Available on Google Play)',
         tags: 'Game, PC, Mobile, Unity3D',
@@ -24,41 +25,47 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/InsatiableSpider2.png",
         url: "https://play.google.com/store/apps/details?id=com.NXTLVL.INSATIABLE_SPIDER",
         year: 2015,
+        haveExternalLink: true
     }, {
         title: '★ Powerful Poptart (In Progress)',
         tags: 'Game, PC, Mobile, Unity3D',
-        description: 'Genetically modified hamster Poptart tries to save his human family from an evil hamster.',
+        description: 'Genetically modified hamster Poptart tries to save his abducted human family.',
         img_url: "img/Projects/PowerfulPoptart.png",
         url: "http://nxtlvlgames.tumblr.com/",
         year: 2015,
+        haveExternalLink: true
     }, {
         title: '★ Into The Light (In Progress)',
         tags: 'Game, PC, Mobile, Unity3D',
-        description: 'The story of a soul who is born in a world of darkness tries to illuminate the world by collecting star dusts',
+        description: 'The story of a soul in the world of darkness tries to illuminate the world by collecting star dusts',
         img_url: "img/Projects/IntoTheLight.png",
         url: "http://nxtlvl.itch.io/into-the-light",
         year: 2015,
+        haveExternalLink: true
     }, {
-        title: 'Forest Runner',
-        tags: 'Game, PC, Mobile, Unity3D',
-        description: 'A story line baesd Infinitly Runner game that details the story of a lone wolf running wild in the forst',
-        img_url: "img/Projects/ForestRunner.png",
-        url: "",
-        year: 2014,
-    }, {
+    //     title: 'Forest Runner',
+    //     tags: 'Game, PC, Mobile, Unity3D',
+    //     description: 'A story line baesd Infinitly Runner game that details the story of a lone wolf running wild in the forst',
+    //     img_url: "img/Projects/ForestRunner.png",
+    //     url: "",
+    //     year: 2014,
+    //     haveExternalLink: true;
+    // }, {
         title: 'Maze Runner',
         tags: 'Puzzld Game, PC, Mobile, Unity3D',
-        description: 'Player switches between first person and third person to solve the maze puzzle',
+        description: 'Player switches between first person and third person to solve maze puzzles',
         img_url: "https://scontent-dfw1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/10616309_767037970035866_2224106353375435192_n.jpg?oh=bee4962b972bda93d5d34b11ff44e879&oe=5635662E",
         url: "",
         year: 2014,
+        haveExternalLink: false
     }, {
         title: 'Entangled',
         tags: 'Web, Front-end, Chrome Experiment, HTML5',
-        description: 'Canvas animation created using HTML5 Canvas, Javascript + LeapMotion.',
+        description: 'Gestured controlled minimalistic canvas animation implemented using LeapMotion motion controller and Javascript.',
         img_url: "img/Projects/Entangled.png",
         url: "https://github.com/AlexOuyang/Canvas-Animation/tree/master/LeapMotion%20Minimalist%20Pattern%20Trials",
         year: 2014,
+        haveExternalLink: true
     }, {
         title: '❦ Image Slider Of Love',
         tags: 'Web, Front-end, HTML5',
@@ -66,6 +73,7 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/ImageSliderOfLove.png",
         url: "http://codepen.io/AlexO/pen/jKEqG",
         year: 2014,
+        haveExternalLink: true
     }, {
         title: 'Gesture Controlled 3D Cube Photo Gallery',
         tags: 'Web, Sinatra, Experiment, Canvas',
@@ -73,6 +81,7 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/Cube.png",
         url: "https://github.com/AlexOuyang/3D-Cube-Gallery",
         year: 2014,
+        haveExternalLink: true
     }, {
         title: '❣ Meca Model - Gabrille',
         tags: 'Art, 3D Modeling, Blender',
@@ -80,6 +89,7 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/meca01.png",
         url: "",
         year: 2014,
+        haveExternalLink: false
     }, {
         title: '❣ Meca Model - Hellsing',
         tags: 'Art, 3D Modeling, Blender',
@@ -87,6 +97,7 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/meca02.png",
         url: "",
         year: 2014,
+        haveExternalLink: false
     }, {
         title: '❣ Meca Model - HellRaiser',
         tags: 'Art, 3D Modeling, Blender',
@@ -94,6 +105,7 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/meca03.png",
         url: "",
         year: 2013,
+        haveExternalLink: false
     }, {
         title: '❣ Meca Model - Blue Elf',
         tags: 'Art, 3D Modeling, Blender',
@@ -101,6 +113,7 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/meca04.png",
         url: "",
         year: 2013,
+        haveExternalLink: false
     }, {
         title: '❣ Meca Model - Pantacle',
         tags: 'Art, 3D Modeling, Blender',
@@ -108,5 +121,6 @@ angular.module('ngFilter').controller("MainController", function() {
         img_url: "img/Projects/meca05.png",
         url: "",
         year: 2013,
+        haveExternalLink: false
     }];
 });
